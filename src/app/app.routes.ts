56c66bcbe13560e6154b607c76b012a21595c32c
sainzing29 <34507094+sainzing29@users.noreply.github.com>
2025-09-15 09:45:38 +0530

    defaukt route changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,7 +3,7 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: '',
-    redirectTo: '/customer/register',
+    redirectTo: '/sign-in',
     pathMatch: 'full'
   },
   // Customer routes (outside admin layout)
