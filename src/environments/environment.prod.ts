97d7fe8d74f41d88537d0e56aa47099f6571adf9
sainzing29 <34507094+sainzing29@users.noreply.github.com>
2026-01-07 23:34:07 +0530

    env changes on railway backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://ceswarranty-backend-production.up.railway.app/api', // Production API base URL
-  appName: 'Coupon Hub',
+  baseUrl: 'https://couponhub-api-production.up.railway.app/api', // Production API base URL
+  appName: 'Ces Coupon',
   version: '1.0.0',
   debug: false,
   recaptchaSiteKey: '6Ldb3SIsAAAAAHRvgoBeObkCWQXqQ5y4n58_0KHq' // Add your reCAPTCHA site key here
